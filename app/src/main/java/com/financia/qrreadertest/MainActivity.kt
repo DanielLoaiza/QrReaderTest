8e7a6adf7fb5b698f7f1175be5b9431ed164998a
Daniel <daniel.loaiza@rappi.com>
2020-06-11 23:51:57 -0500

    qr code test

---
+++ app/src/main/java/com/financia/qrreadertest/MainActivity.kt
@@ -0,0 +1,176 @@
+package com.financia.qrreadertest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode.TYPE_URL
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode.TYPE_WIFI
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
+
+
+// This is an arbitrary number we are using to keep track of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts.
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class MainActivity : AppCompatActivity(), LifecycleOwner, CameraXConfig.Provider {
+
+
+    private val executor = Executors.newSingleThreadExecutor()
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        if (allPermissionsGranted()) {
+            view_finder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig()
+    }
+
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                view_finder.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    fun scanBarcodes(image: FirebaseVisionImage) {
+        // [START set_detector_options]
+        val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+            .setBarcodeFormats(
+                FirebaseVisionBarcode.FORMAT_ALL_FORMATS
+            )
+            .build()
+        // [END set_detector_options]
+
+        // [START get_detector]
+        val detector = FirebaseVision.getInstance()
+            .getVisionBarcodeDetector(options)
+        // Or, to specify the formats to recognize:
+        // val scanner = BarcodeScanning.getClient(options)
+        // [END get_detector]
+
+        // [START run_detector]
+        val result = detector.detectInImage(image)
+            .addOnSuccessListener { barcodes ->
+                // Task completed successfully
+                // [START_EXCLUDE]
+                // [START get_barcodes]
+                for (barcode in barcodes) {
+                    val bounds = barcode.boundingBox
+                    val corners = barcode.cornerPoints
+
+                    val rawValue = barcode.rawValue
+                    Log.e("barcodes", rawValue)
+
+                    val valueType = barcode.valueType
+                    // See API reference for complete list of supported types
+                    when (valueType) {
+                        TYPE_WIFI -> {
+                            val ssid = barcode.wifi!!.ssid
+                            val password = barcode.wifi!!.password
+                            val type = barcode.wifi!!.encryptionType
+                        }
+                        TYPE_URL -> {
+                            val title = barcode.url!!.title
+                            val url = barcode.url!!.url
+                        }
+                    }
+                }
+                // [END get_barcodes]
+                // [END_EXCLUDE]
+            }
+            .addOnFailureListener {
+                // Task failed with an exception
+                // ...
+                Log.e("error", it.message)
+            }
+        // [END run_detector]
+    }
+
+    private fun startCamera() {
+
+        // Build the viewfinder use case
+        val preview = Preview.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        view_finder.preferredImplementationMode = PreviewView.ImplementationMode.SURFACE_VIEW
+        preview.setSurfaceProvider(view_finder.createSurfaceProvider())
+
+        val analyzerUseCase = ImageAnalysis.Builder().apply {
+            setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+        }.build()
+        analyzerUseCase.setAnalyzer(executor, ImageAnalyzer(this))
+
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        val camera = cameraProviderFuture.get()
+            .bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, analyzerUseCase)
+
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+}
